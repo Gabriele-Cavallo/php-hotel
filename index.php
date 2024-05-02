8968b994a69342bf8bd04df5aba9358ea800eef7
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-02 14:33:41 +0200

    feat: add Bootstrap table

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
 
     ];
     $parking;
+    $hotelInfo = ['Hotels', 'Description', 'Parking available', 'Users vote', 'Distance to center'];
 ?>
 
 <!DOCTYPE html>
@@ -52,6 +53,7 @@
 </head>
 <body>
     <h1>ECCO I NOSTRI HOTEL</h1>
+    <!-- Hotels simple list -->
     <ul>
         <?php foreach ($hotels as $hotel) {?>
             <li>
@@ -63,5 +65,29 @@
             </li>
         <?php } ?>
     </ul>
+    <!-- /Hotels simple list -->
+
+    <!-- Hotels Table -->
+    <table class="table">
+        <thead class="table-dark">
+            <tr>
+                <?php foreach($hotelInfo as $singleInfo) { ?>
+                    <th class="text-center" scope="col"><?php echo $singleInfo; ?></th>
+                <?php } ?>
+            </tr>
+        </thead>
+        <tbody class="table-secondary">
+            <?php foreach($hotels as $hotel) { ?>
+                <tr class="text-center">
+                    <th scope="row"><?php echo $hotel['name']; ?></th>
+                    <td><?php echo $hotel['description']; ?></td>
+                    <td><?php echo $parking = $hotel['parking'] ? 'available' : 'not available'; ?></td>
+                    <td><?php echo $hotel['vote']; ?>/5</td>
+                    <td><?php echo $hotel['distance_to_center']; ?> km</td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    <!-- /Hotels Table -->
 </body>
 </html>
